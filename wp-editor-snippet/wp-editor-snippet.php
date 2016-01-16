4d5176c6f2ed333f553e84bf51fec2acef027841
MediaVita <ajit@developmentbucket.com>
2016-01-16 11:20:31 +0530

    Update wp-editor-snippet.php

--- wp-editor-snippet/wp-editor-snippet.php
+++ wp-editor-snippet/wp-editor-snippet.php
@@ -1,1 +1,11 @@
-
+<?php
+/**
+ * Plugin Name: WP Editor Snippet
+ * Plugin URI: http://www.themevita.com
+ * Description: Convert Custom post type content to shortcodes your can use Wordpress Editor, siteorigin page builder or Visual Composer for Wordpress
+ * Version: 1.0.0
+ * Author: Ajit Bhandari
+ * Author URI: http://www.themevita.com
+ * License: GPL2
+ */
+ ?>
